633fba0a3ef7582462b2c3739181efc1c8d1c560
duong dang <dhduongt3@gmail.com>
2020-11-24 06:27:18 +0700

    Initial Commit

---
+++ 3DDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3DDemo
+//
+//  Created by dang huu duong on 11/24/20.
+//  Copyright © 2020 Compathnion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
